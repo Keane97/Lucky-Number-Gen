f576a8d066f6cd36e696e034d8b97db7c33f1c01
Keane97 <liamkeane49@yahoo.com>
2023-07-21 20:27:46 +0100

    Passed entered name between two activities and displayed in toast

--- app/src/main/java/com/example/luckynumberapp/LuckyNumberActivity.java
+++ app/src/main/java/com/example/luckynumberapp/LuckyNumberActivity.java
@@ -2,9 +2,11 @@ package com.example.luckynumberapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class LuckyNumberActivity extends AppCompatActivity {
 
@@ -19,5 +21,12 @@ public class LuckyNumberActivity extends AppCompatActivity {
         welcomeText = findViewById(R.id.txtTitle);
         luckyNumberTxt = findViewById(R.id.luckyNumbertext);
         share_btn = findViewById(R.id.share_number_btn);
+
+        Intent intent = getIntent();
+
+        String userName = intent.getStringExtra("name");
+
+        Toast.makeText(this, "Username: " + userName, Toast.LENGTH_SHORT).show();
+
     }
 }
--- app/src/main/java/com/example/luckynumberapp/MainActivity.java
+++ app/src/main/java/com/example/luckynumberapp/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.luckynumberapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -20,6 +22,22 @@ public class MainActivity extends AppCompatActivity {
         textView = findViewById(R.id.txtTitle);
         editText = findViewById(R.id.edtEntername);
         btn = findViewById(R.id.btnSignIn);
+
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username = editText.getText().toString();
+
+                // Explicit Intent.
+                Intent intent = new Intent(getApplicationContext(), LuckyNumberActivity.class);
+
+                //Passing the name to second activity
+                intent.putExtra("name", username);
+
+                startActivity(intent);
+            }
+        });
 
 
     }
