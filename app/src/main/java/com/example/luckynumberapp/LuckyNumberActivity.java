8e1c3a7d20d8b501125b43e0097053f0f2222891
Keane97 <liamkeane49@yahoo.com>
2023-07-21 15:43:03 +0100

    Creation of variables required for comms between activities.

--- app/src/main/java/com/example/luckynumberapp/LuckyNumberActivity.java
+++ app/src/main/java/com/example/luckynumberapp/LuckyNumberActivity.java
@@ -3,12 +3,21 @@ package com.example.luckynumberapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class LuckyNumberActivity extends AppCompatActivity {
+
+    TextView welcomeText, luckyNumberTxt;
+    Button share_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lucky_number);
+
+        welcomeText = findViewById(R.id.txtTitle);
+        luckyNumberTxt = findViewById(R.id.luckyNumbertext);
+        share_btn = findViewById(R.id.share_number_btn);
     }
 }
--- app/src/main/java/com/example/luckynumberapp/MainActivity.java
+++ app/src/main/java/com/example/luckynumberapp/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.luckynumberapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText editText;
+    Button btn;
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.txtTitle);
+        editText = findViewById(R.id.edtEntername);
+        btn = findViewById(R.id.btnSignIn);
+
+
     }
 }
